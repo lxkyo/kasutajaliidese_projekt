1099151a0dc243c6b973e594ccc5090288a950ca
lxkyo <90316779+lxkyo@users.noreply.github.com>
2023-03-21 03:39:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-window.onscroll = () => {
-    const nav = document.querySelector('#navbar');
-    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
 };
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries, 
+    sectionOneObserver
+) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            header.classList.add("nav-scrolled");
+        } else {
+            header.classList.remove("nav-scrolled");
+        }
+    })
+}, sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne); 
+
+
+
+console.log('Hello world')
+
+
+$('#myDropdown').on('show.bs.dropdown', function () {
+    // do something...
+})
+
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+   alert("You clicked me");
+})
